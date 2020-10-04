732b33807e860cf84332cc611d2732a2cd1912e9
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2020-10-04 14:33:06 -0400

    refactorizado

--- js/index.js
+++ js/index.js
@@ -1,94 +1,104 @@
-window.onload = () => {
-    callAPI();
-    document.querySelector("form").addEventListener("submit", search, false);
+window.onload = async () => {
+    await callAPI();
+    gridLayout();
+    components = document.querySelectorAll(".block");
+    document.querySelector("#mode").addEventListener("click", mode, false);
+    document.querySelector("#form").addEventListener("submit", search, false);
     document.querySelector("#search").addEventListener("click", search, false);
+    document.querySelector("#select").addEventListener("change", filter, false);
+    window.addEventListener("resize", gridLayout, false);
 };
-
-window.onresize = gridLayout;
 
 var components;
 var position;
 
 async function callAPI() {
     try {
-        const response = await fetch("https://restcountries.eu/rest/v2/all", {
-            cache: "default",
-        });
+        const response = await fetch("https://restcountries.eu/rest/v2/all");
         const data = await response.json();
         await showComponents(data);
-        gridLayout();
-        getRegions(data);
-        components = document.querySelectorAll(".block");
+        showRegions(data);
     } catch (error) {
         console.log(error);
     }
 }
 
-async function showComponents(object) {
-    html = object.map((element) => createComponents(element, object)).join("");
-    document.querySelector("#container").innerHTML = html;
+async function showComponents(data) {
+    document.querySelector("#container").innerHTML = await createComponents(
+        data
+    );
 
-    function createComponents(element, object) {
-        let borders = object
-            .filter((obj) => element.borders.includes(obj.alpha3Code))
-            .map((obj) => {
-                if (obj) {
-                    return `<span onclick='showDetails(this, false)' data-name='${obj.name}'>${obj.name}</span>`;
-                }
+    async function createComponents(object) {
+        let html = object
+            .map((element) => {
+                let borders = object
+                    .filter((obj) => element.borders.includes(obj.alpha3Code))
+                    .map((obj) => {
+                        if (obj) {
+                            return `<span onclick='showDetails(this, false)' 
+                            data-name=&quot;${obj.name}&quot;>${obj.name}</span>`;
+                        }
+                    })
+                    .join("");
+
+                let dataset = `
+                    data-name="${element.name}" 
+                    data-flag="${element.flag}" 
+                    data-capital="${element.capital}"
+                    data-region="${element.region}" 
+                    data-subregion="${element.subregion}"
+                    data-languages="${[
+                        ...element.languages.map((lan) => lan.name),
+                    ]}" 
+                    data-population="${element.population.toLocaleString(
+                        "es-MX"
+                    )}" 
+                    data-currencies="${[
+                        ...element.currencies.map((cur) => cur.name),
+                    ]}"
+                    data-borders="${borders}"`;
+
+                return `
+                    <div id="${
+                        element.name
+                    }" class="block" onclick="showDetails(this, true)" ${dataset}>
+                        <div>
+                            <img src="${element.flag}" alt="Flag">
+                        </div>
+                        <ul>
+                            <li>${element.name}</li>
+                            <li>Capital: ${element.capital}</li>
+                            <li>Region: ${element.region}</li>
+                            <li>Population: ${element.population.toLocaleString(
+                                "es-MX"
+                            )}</li>
+                        </ul>
+                    </div>`;
             })
             .join("");
 
-        let dataset = `
-        data-name="${element.name}" 
-        data-flag="${element.flag}" 
-        data-capital="${element.capital}"
-        data-region="${element.region}" 
-        data-subregion="${element.subregion}"
-        data-languages="${[...element.languages.map((lan) => lan.name)]}" 
-        data-population="${element.population.toLocaleString("es-MX")}" 
-        data-currencies="${[...element.currencies.map((cur) => cur.name)]}"
-        data-borders="${borders}"`;
-
-        html = `
-        <div id="${
-            element.name
-        }" class="block" onclick="showDetails(this, true)" ${dataset}>
-            <div>
-                <img src="${element.flag}" alt="Flag">
-            </div>
-            <ul>
-                <li>${element.name}</li>
-                <li>Capital: ${element.capital}</li>
-                <li>Region: ${element.region}</li>
-                <li>Population: ${element.population.toLocaleString(
-                    "es-MX"
-                )}</li>
-            </ul>
-        </div>`;
         return html;
     }
 }
 
-async function getRegions(data) {
-    let regions = [...new Set(data.map((element) => element.region))];
-    let subregions = [...new Set(data.map((element) => element.subregion))];
+async function showRegions(data) {
+    document.querySelector("#select").innerHTML = await creatRegion(data);
 
-    setRegion(regions, "region");
-    setRegion(subregions, "subregion");
+    async function creatRegion(data) {
+        let region = [...new Set(data.map((element) => element.region))];
+        let subregion = [...new Set(data.map((element) => element.subregion))];
+        let regions = region.concat(subregion).sort();
 
-    document
-        .querySelector("select")
-        .addEventListener("change", filterRegion, false);
-
-    function setRegion(regions, optgroup) {
-        let html = regions
+        let html = `<option value="">All</option>`;
+        html += regions
             .map((element) => {
                 if (element) {
                     return `<option value="${element}">${element}</option>`;
                 }
             })
             .join("");
-        document.getElementById(optgroup).innerHTML = html;
+
+        return html;
     }
 }
 
@@ -98,14 +108,15 @@ async function gridLayout() {
     let cell = document.querySelector(".block");
     let cellMargin = parseInt(window.getComputedStyle(cell).margin);
     let space = parseInt(
-        section.clientWidth / ((cell.offsetWidth || 220) + cellMargin)
+        section.clientWidth /
+            (parseInt(window.getComputedStyle(cell).width) + cellMargin)
     );
     grid.style.gridTemplateColumns = `repeat(${space}, 1fr)`;
 }
 
-function filterRegion(e) {
+async function filter(e) {
     let option = e.target.options[e.target.selectedIndex].value;
-    let coincidense = new Array(...components)
+    let coincidense = Array.from(components)
         .filter(
             (obj) =>
                 obj.dataset.region.includes(option) ||
@@ -122,7 +133,7 @@ function filterRegion(e) {
     });
 }
 
-function changeMode() {
+async function mode() {
     let body = document.querySelector("body");
     let bgColor = window.getComputedStyle(body).backgroundColor;
     if (bgColor === "rgb(224, 224, 224)") {
@@ -139,7 +150,7 @@ function changeMode() {
     }
 }
 
-function search(e) {
+async function search(e) {
     e.preventDefault();
 
     let search = document
@@ -159,11 +170,7 @@ function search(e) {
 }
 
 function showDetails(country, scroll = true) {
-    let element = new Array(...components).find(
-        (object) => object.dataset.name === country.dataset.name
-    ).dataset;
-
-    createDetails(element);
+    document.querySelector("#details").innerHTML = createDetails(country);
 
     if (scroll) {
         position = window.scrollY;
@@ -174,38 +181,46 @@ function showDetails(country, scroll = true) {
     document.querySelector("#main-content").style.display = "none";
     document.querySelector("#detail-content").style.display = "block";
 
-    function createDetails(element) {
-        document.querySelector("#details").innerHTML = `
-    <div id="back" onclick="back()">Back</div>
-    <div id="flex-details">
-        <div id="img">
-            <img src="${element.flag}" alt="Flag of ${element.name}">
-        </div>
-        <div id="container-data">
-            <p>${element.name}</p>
-            <div id="data">
-                <ul>
-                    <li><span>Capital:</span> ${element.capital}</li>
-                    <li><span>Region:</span> ${element.region}</li>
-                    <li><span>Sub Region:</span> ${element.subregion}</li>
-                </ul>
-                <ul>
-                    <li><span>Language:</span> ${element.languages}</li>
-                    <li><span>Population:</span> ${element.population.toLocaleString(
-                        "es-MX"
-                    )}</li>
-                    <li><span>Currencies:</span> ${element.currencies}</li>
-                </ul>
-            </div>
-            <ul id="borders">
-                <li>Borders: ${element.borders}</li>
-            </ul>
-        </div>
-    </div>`;
+    function createDetails(country) {
+        let element = Array.from(components).find(
+            (object) => object.dataset.name === country.dataset.name
+        ).dataset;
+
+        return `
+            <div id="back" onclick="back()">Back</div>
+            <div id="flex-details">
+                <div id="img">
+                    <img src="${element.flag}" alt="Flag of ${element.name}">
+                </div>
+                <div id="container-data">
+                    <p>${element.name}</p>
+                    <div id="data">
+                        <ul>
+                            <li><span>Capital:</span> ${element.capital}</li>
+                            <li><span>Region:</span> ${element.region}</li>
+                            <li><span>Sub Region:</span> ${
+                                element.subregion
+                            }</li>
+                        </ul>
+                        <ul>
+                            <li><span>Language:</span> ${element.languages}</li>
+                            <li><span>Population:</span> ${element.population.toLocaleString(
+                                "es-MX"
+                            )}</li>
+                            <li><span>Currencies:</span> ${
+                                element.currencies
+                            }</li>
+                        </ul>
+                    </div>
+                    <ul id="borders">
+                        <li>Borders: ${element.borders}</li>
+                    </ul>
+                </div>
+            </div>`;
     }
 }
 
-function back() {
+async function back() {
     document.querySelector("#detail-content").style.display = "none";
     document.querySelector("#section-form").style.display = "flex";
     document.querySelector("#main-content").style.display = "flex";
