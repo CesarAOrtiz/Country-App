06f68e89dbb4fb06b163883992aa67326a1ec55f
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2020-10-31 04:02:17 -0400

    optimizando

--- js/index.js
+++ js/index.js
@@ -24,13 +24,28 @@ function getMedia() {
     getMode();
 }
 
+function getMode() {
+    let mode = document.querySelector("#mode");
+    if (document.body.className === "dark-theme") {
+        mode.innerHTML = "Light Mode";
+    } else {
+        mode.innerHTML = "Dark Mode";
+    }
+}
+
+async function toggleMode() {
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
+    getMode();
+}
+
 async function callAPI() {
     try {
         const response = await fetch(
             "https://restcountries.eu/rest/v2/all?fields=name;capital;currencies;alpha3Code;region;subregion;flag;population;borders;languages"
         );
         const data = await response.json();
-        showComponents(data);
+        showCards(data);
         showRegions(data);
         gridLayout();
         countries = data;
@@ -39,16 +54,16 @@ async function callAPI() {
     }
 }
 
-function showComponents(data) {
-    document.querySelector("#container").innerHTML = createComponents(data);
+function showCards(data) {
+    document.querySelector("#container").innerHTML = createCards(data);
 
-    function createComponents(object) {
+    function createCards(object) {
         let html = object
             .map(
                 (element) => `
                     <div id="${
                         element.name
-                    }" class="block" onclick="showDetails(this, true)">
+                    }" class="card" onclick="showDetails(this, true)">
                         <img width=220px hieght=150px src="${
                             element.flag
                         }" alt="Flag">
@@ -90,7 +105,7 @@ function showRegions(data) {
 function gridLayout() {
     let section = document.querySelector("#main-content");
     let grid = document.querySelector("#container");
-    let cell = document.querySelector(".block");
+    let cell = document.querySelector(".card");
     let cellMargin = parseInt(window.getComputedStyle(cell).margin);
     let space = parseInt(
         section.clientWidth /
@@ -109,7 +124,7 @@ function search(e) {
     const coincidense = countries.filter((object) =>
         object.name.toLowerCase().includes(search)
     );
-    showComponents(coincidense);
+    showCards(coincidense);
 
     window.scroll(0, 0);
 }
@@ -119,27 +134,12 @@ function filter(e) {
     const coincidense = countries.filter(
         (obj) => obj.region.includes(option) || obj.subregion.includes(option)
     );
-    showComponents(coincidense);
+    showCards(coincidense);
 
     window.scroll(0, 0);
 }
 
-function toggleMode() {
-    document.body.classList.toggle("dark-theme");
-    document.body.classList.toggle("light-theme");
-    getMode();
-}
-
-function getMode() {
-    let mode = document.querySelector("#mode");
-    if (document.body.className === "dark-theme") {
-        mode.innerHTML = "Light Mode";
-    } else {
-        mode.innerHTML = "Dark Mode";
-    }
-}
-
-async function showDetails(country, scroll = true) {
+function showDetails(country, scroll = true) {
     const element = countries.find((obj) => obj.name === country.id);
     const borders = countries.filter((obj) =>
         element.borders.includes(obj.alpha3Code)
@@ -150,7 +150,7 @@ async function showDetails(country, scroll = true) {
             (borderNames += `<span onclick="showDetails(this, false)" id="${obj.name}" class="border">${obj.name}</span>`)
     );
 
-    document.querySelector("#detail-content").innerHTML = await createDetails(
+    document.querySelector("#detail-content").innerHTML = createDetails(
         element,
         borderNames
     );
@@ -165,7 +165,7 @@ async function showDetails(country, scroll = true) {
     document.querySelector("#main-content").style.display = "none";
     document.querySelector("#detail-content").style.display = "block";
 
-    async function createDetails(element, borders) {
+    function createDetails(element, borders) {
         return `
     <div id="back" onclick="back()">Back</div>
     <div id="flex-details">
